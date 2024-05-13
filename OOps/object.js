36a6f5bc70846d69d09721498b917299bf7fe8af
Nilu patel <nilusingh945164@gmail.com>
2024-05-13 14:57:49 +0530

    .

---
+++ OOps/object.js
@@ -0,0 +1,10 @@
+// A javaScript object is an entity having state and behavior (properties and method).
+//  For example: car, pen, bike, chair, glass, keyboard, monitor etc.
+
+// There are 3 ways to create objects.
+/**
+ * By object literal
+ * By creating instance of Object directly (using new keyword)
+  *By using an object constructor (using new keyword)
+
+ */
